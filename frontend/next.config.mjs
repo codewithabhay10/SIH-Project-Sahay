c7d4315c2dc8e369f6c2fd619c0641c098605b7e
codewithabhay10 <abhaymadan22@gmail.com>
2026-02-04 20:06:02 +0530

    chore: update dependencies and enhance Next.js configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -12,6 +12,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  experimental: {
+    allowDevelopmentBuild: true,
+  },
   output: "standalone",
   outputFileTracingRoot: __dirname,
 };
